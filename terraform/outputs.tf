487c146586c79c96d2db94640ddf84c6aa3c787e
Artem Borysenko1 <artem_borysenko1@epam.com>
2021-06-13 11:58:17 +0300

    including terraform version requirements and google  provider version definitions

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,16 +2,10 @@ output "bookshelfinstance_ip" {
   value = google_compute_instance_from_template.tpl.network_interface.0.network_ip
 }
 
-output "google_sql_database_instance_ip" {
-  value = google_sql_database_instance.instance.ip_address.0.ip_address
-}
 output "bookshelfinstance_id" {
   value = google_compute_instance_from_template.tpl.id
 }
 
-output "google_sql_database_instance_id" {
-  value = google_sql_database_instance.instance.id
-}
 
 output "instance_service_account" {
   description = "Service account for instances."
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+
+terraform {
+  required_version = ">=0.15.4"
+  required_providers {
+    google = ">= 3.43, <4.0"
+  }
+}
 provider "google" {
-  project = "my-gcp-terraform"
+  #  credentials = "${file("account.json")}"
+  project = var.project_id
+  region  = var.region
 }
